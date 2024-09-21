bc54f0a95d805eab431334fe21efc991129b2e9a
Villode <vip520moyan@gmail.com>
2024-09-21 22:02:58 +0800

    更新导航栏中文

--- client/src/page/essay.tsx
+++ client/src/page/essay.tsx
@@ -1,18 +1,17 @@
 import { Helmet } from 'react-helmet';
-import { useTranslation } from 'react-i18next';
 
 export const EssayPage = () => {
-  const { t } = useTranslation(); // 使用国际化支持
-
   return (
     <>
       <Helmet>
-        <title>{t('essay')}</title> {/* 使用翻译字符串作为标题 */}
+        <title>Essay</title>
       </Helmet>
       <div>
-        <h1>{t('essay')}</h1> {/* 使用翻译字符串 */}
-        <p>{t('这里是说说')}</p> {/* 也可以将这里的文本放入翻译文件中 */}
+        <h1>Essay</h1>
+        <p>这里是说说</p>
       </div>
     </>
   );
 };
+
+
