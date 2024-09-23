c749499e97f9fb0663cfce9632c0bb54f75876d0
Villode <vip520moyan@gmail.com>
2024-09-23 16:10:35 +0800

    去除黑白切换加载默认为黑
    去除黑白切换加载默认为黑

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -1,37 +1,22 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect } from 'react';
 import Popup from 'reactjs-popup';
 import { ClientConfigContext } from '../state/config';
 import { Helmet } from "react-helmet";
 import { siteName } from '../utils/constants';
 import { useTranslation } from "react-i18next";
 
-type ThemeMode = 'light' | 'dark' | 'system';
+
 function Footer() {
-    const { t } = useTranslation()
-    const [modeState, setModeState] = useState<ThemeMode>('system');
+    const { t } = useTranslation();
     const config = useContext(ClientConfigContext);
     const footerHtml = config.get<string>('footer');
+
     useEffect(() => {
-        const mode = localStorage.getItem('theme') as ThemeMode || 'system';
-        setModeState(mode);
-        setMode(mode);
-    }, [])
+        setDarkMode();
+    }, []);
 
-    const setMode = (mode: ThemeMode) => {
-        setModeState(mode);
-        localStorage.setItem('theme', mode);
-
-
-        if (mode !== 'system' || (!('theme' in localStorage) && window.matchMedia(`(prefers-color-scheme: ${mode})`).matches)) {
-            document.documentElement.setAttribute('data-color-mode', mode);
-        } else {
-            const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
-            if (mediaQuery.matches) {
-                document.documentElement.setAttribute('data-color-mode', 'dark');
-            } else {
-                document.documentElement.setAttribute('data-color-mode', 'light');
-            }
-        }
+    const setDarkMode = () => {
+        document.documentElement.setAttribute('data-color-mode', 'dark');
         window.dispatchEvent(new Event("colorSchemeChange"));
     };
 
@@ -78,11 +63,6 @@ function Footer() {
                         </Popup>
                     </>}
                 </p>
-                <div className="w-fit-content inline-flex rounded-full border border-zinc-200 p-[3px] dark:border-zinc-700">
-                    <ThemeButton mode='light' current={modeState} label="Toggle light mode" icon="ri-sun-line" onClick={setMode} />
-                    <ThemeButton mode='system' current={modeState} label="Toggle system mode" icon="ri-computer-line" onClick={setMode} />
-                    <ThemeButton mode='dark' current={modeState} label="Toggle dark mode" icon="ri-moon-line" onClick={setMode} />
-                </div>
             </div>
         </footer>
     );
@@ -92,14 +72,7 @@ function Spliter() {
     return (<span className='px-1'>
         |
     </span>
-    )
-}
-
-function ThemeButton({ current, mode, label, icon, onClick }: { current: ThemeMode, label: string, mode: ThemeMode, icon: string, onClick: (mode: ThemeMode) => void }) {
-    return (<button aria-label={label} type="button" onClick={() => onClick(mode)}
-        className={`rounded-inherit inline-flex h-[32px] w-[32px] items-center justify-center border-0 t-primary ${current === mode ? "bg-w rounded-full shadow-xl shadow-light" : ""}`}>
-        <i className={`${icon}`} />
-    </button>)
+    );
 }
 
 export default Footer;
